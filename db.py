b595cd713f7f290a714bb5f3e01e9dd9e206ba65
Emilly <emillyquessada26@gmail.com>
2025-09-30 16:45:06 -0300

    Add função de conectar ✔

--- db.py
+++ db.py
@@ -13,3 +13,13 @@ params = {
     "host": os.getenv("DB_HOST"),
     "port": os.getenv("DB_PORT"),
 }
+
+def conectar():
+    try:
+        conexao = pg.connect(**params)
+        cursor = conexao.cursor()
+        return conexao, cursor
+    except Exception as erro:
+        print(f"Erro de conexão {erro}")
+        return None, None
+    
